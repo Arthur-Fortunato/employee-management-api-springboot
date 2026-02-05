c69140b751a86de911ace34d8e8147089ab67575
Arthur-Fortunato <150343091+Arthur-Fortunato@users.noreply.github.com>
2026-02-05 13:33:12 -0300

    Add Employee endpoints
    * feat: add EmployeeController with GET endpoints
    
    * feat: add employee creation endpoint
    
    * feat: add employee update endpoint
    
    * feat: add employee delete endpoint

---
+++ src/main/java/com/infnet/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.infnet.employeemanagement.controller;
+
+import com.infnet.employeemanagement.entity.Employee;
+import com.infnet.employeemanagement.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
